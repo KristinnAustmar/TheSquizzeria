1b62e080bf141a2f05380379d35a07c80986127c
Kjartan97 <kjartan.reynir.olafsson@gmail.com>
2017-11-29 12:59:37 +0000

    breytti main menu og klasa

---
+++ klasar_fyrir_pizza/src/main_ui.cpp
@@ -0,0 +1,25 @@
+#include "main_ui.h"
+
+main_ui::main_ui()
+{
+    //ctor
+}
+
+main_ui::~main_ui()
+{
+    //dtor
+}
+void MainUI::startUI() {
+    char selection = '0';
+    while(selection != 'q') {
+        cout << "p : make pizza" << endl;
+        cout << "w : write " << endl;
+        cout << "r : read " << endl;
+        cout << "w : quit " << endl;
+
+    }
+
+
+
+
+})
