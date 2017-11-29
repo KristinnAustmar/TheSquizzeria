cf4278e37db28f4f554949060647e6cbc77d5f60
KristinnAustmar <kristinn.austmar@gmail.com>
2017-11-29 13:54:34 +0000

    Comment

---
+++ PizzaPizza/PiazzaUI.h
@@ -0,0 +1,14 @@
+#ifndef PIAZZAUI_H
+#define PIAZZAUI_H
+
+
+class PiazzaUI
+{
+    public:
+        PiazzaUI();
+        virtual ~PiazzaUI();
+    protected:
+    private:
+};
+
+#endif // PIAZZAUI_H
---
+++ PizzaPizza/Pizza.h
@@ -0,0 +1,14 @@
+#ifndef PIZZA_H
+#define PIZZA_H
+
+
+class Pizza
+{
+    public:
+        Pizza();
+        virtual ~Pizza();
+    protected:
+    private:
+};
+
+#endif // PIZZA_H
